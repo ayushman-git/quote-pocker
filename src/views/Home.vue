ea51278ef8d6ee323da43d95b667b5c5b9233cbb
Ayushman Gupta <ayushman1998.1998@gmail.com>
2021-04-16 13:04:01 +0530

    init quote pocket, added tailwind

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section class="grid place-items-center min-h-screen">
+    <section>
+      <header class="flex items-center gap-3 border-b-2 mb-8 pb-2 border-black">
+        <h1 class="text-3xl md:text-4xl font-bold">Quote Pocket</h1>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+          />
+        </svg>
+      </header>
+      <div class="text-base">
+        <h2>Log in</h2>
+      </div>
+    </section>
+  </section>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
